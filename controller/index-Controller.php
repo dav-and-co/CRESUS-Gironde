420c18c4e0bf74f625db1b2e666518551018ec79
dav-and-co <claude.daviatte@lapiscine.pro>
2024-07-04 15:34:35 +0200

    éléments au 04 07 2024

---
+++ controller/index-Controller.php
@@ -0,0 +1,47 @@
+<?php
+$activites=[
+    [
+        'title' => 'Education budgétaire',
+        'in2lines'=> 'Education <br> budgétaire',
+        'content' => '',
+        'alt'=>'logo jeu Dilemme',
+        'img-gde' => "../assets/img/logo_Dilemme-fond-blanc-petit.png",
+        'img-pte' => "../assets/img/logo_Dilemme-fond-blanc-petit.png",
+        'ancre'=>'#dilemme',
+    ],
+    [
+        'title' => 'Accompagnement budgétaire',
+        'in2lines'=> 'Accompagnement <br> budgétaire',
+        'content' => '',
+        'alt'=>'logo budget',
+        'img-gde' =>"../assets/img/picto-calculette.png",
+        'img-pte' => "../assets/img/picto-calculette.png",
+        'ancre'=>'#budget',
+    ],
+    [
+        'title' => 'Accompagnement surendettement',
+        'in2lines'=> 'Accompagnement <br> surendettement',
+        'content' => '',
+        'alt'=>'logo surendettement',
+        'img-gde' =>"../assets/img/picto-surendettement.png",
+        'img-pte' => "../assets/img/picto-surendettement.png",
+        'ancre'=>'#surdet',
+    ],
+    [
+        'title' => 'Accompagnement microcrédit',
+        'in2lines'=> 'Accompagnement <br> microcrédit',
+        'content' => '',
+        'alt'=>'logo microcredit',
+        'img-gde' =>"../assets/img/picto-micro-credit.png",
+        'img-pte' => "../assets/img/picto-micro-credit.png",
+        'ancre'=>'#credit',
+    ]
+];
+
+
+
+
+
+
+
+?>
---
+++ views/action.php
@@ -0,0 +1,21 @@
+<!--            http://localhost/projet_cd/views/           -->
+
+
+    <!-- appel de errorConfig et index.controleur  -->
+<?php require_once('../config/errorConfig.php'); ?>
+<?php require_once('../controller/index-Controller.php'); ?>
+
+    <!-- appel du header  -->
+<?php require_once('../partials/headerGP.php'); ?>
+
+<main>
+<h1>Faisons le point de votre situation pour trouver des solutions</h1>
+
+<p>Dans un accompagnement budgétaire individuel et confidentiel, une personne ou un couple en difficulté financière bénéficie des conseils d'un bénévole sous la forme d’un suivi personnalisé dans l'une de nos permanences, exclusivement sur Rendez vous.</p>
+<p>Nous établissons avec vous votre budget et nous vous aidons à trouver des solutions adéquates.</p>
+
+
+</main> 
+
+    <!-- appel du footer  -->
+<?php require_once('../partials/footer.php'); ?>
